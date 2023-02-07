49d64cc6a5b1d29c5391455ea3eae6511ed8ec0d
fishdaa <fishda456@gmail.com>
2023-02-07 21:18:59 +0800

    added more descriptions on functions

--- src/TaskManager.h
+++ src/TaskManager.h
@@ -3,39 +3,46 @@
 
 #include <Arduino.h>
 
-typedef void (*TaskFunc)();
 
 class TaskManager {
   public:
     TaskManager(uint8_t maxTasks) : tasks(new Task[maxTasks]) { this->maxTasks = maxTasks; }
     ~TaskManager() { delete[] tasks; }
 
-    // add a task that will run indefinitely
-    uint8_t addTask(uint32_t interval, TaskFunc func);
+    /// @brief Add a task that will run indefinitely.
+    /// @param interval The time before the task is executed.
+    /// @param func The task to execute.
+    /// @return The task id.
+    uint8_t addTask(uint32_t interval, void (*func)());
 
-    // add a task that will run for a specified number of times
-    uint8_t addTask(uint32_t interval, uint16_t times, TaskFunc func);
+    /// @brief Add a task that will run for a specified number of times
+    /// @param interval The time before the task is executed.
+    /// @param iterations The number of times the task is executed before it is removed.
+    /// @param func The task to execute.
+    /// @return The task id.
+    uint8_t addTask(uint32_t interval, uint16_t iterations, void (*func)());
 
-    // remove a task from the array of tasks
+    /// @brief Remove a task from the array of tasks
+    /// @param taskId The task id to be removed.
     void removeTask(uint8_t taskId);
 
-    // update all the tasks in the TaskManager
+    /// @brief Updates all the tasks in the TaskManager
     void update();
     
   private:
-    // this struct holds a function, its associated interval, and the number of times it should run
+    // This struct holds a function, its associated interval, and the number of times it should run
     struct Task {
       uint8_t taskId;
-      TaskFunc func;
+      void (*func)();
       uint32_t interval;
       uint32_t previousMillis;
       uint16_t times;
       bool repeat;
     };
 
-    uint8_t maxTasks = 10; // the maximum number of tasks the TaskManager can hold
-    Task* tasks; // a dynamic array to hold the tasks
-    uint8_t taskCount = 0; // the number of tasks in the array
+    uint8_t maxTasks = 10; // The maximum number of tasks the TaskManager can hold
+    Task* tasks; // A dynamic array to hold the tasks
+    uint8_t taskCount = 0; // The number of tasks in the array
 };
 
 #endif
